2a9fa7419dc27cab2222f5f9b8345f6222872e8f
Toewaioo <sawtoewaioo2003@gmail.com>
2025-07-21 11:30:40 +0630

    fixed some errors

--- index.php
+++ index.php
@@ -3,8 +3,8 @@ declare(strict_types=1);
 
 define('BASE_PATH', dirname(__DIR__)); // BASE_PATH = project-root
 
-require BASE_PATH . '/vendor/autoload.php';
-$config = require BASE_PATH . '/src/config.php';
+require '/app/vendor/autoload.php';
+$config = require '/app/src/config.php';
 
 $app = new App\Core\Application($config);
 $app->run();
