c88dd978cfe771fee3850971be90648212b4df99
Toewaioo <sawtoewaioo2003@gmail.com>
2025-07-21 11:13:16 +0630

    fixed some error

--- index.php
+++ index.php
@@ -5,15 +5,13 @@ declare(strict_types=1);
 // Project root
 define('BASE_PATH', dirname(__DIR__));
 
-var_dump(__DIR__);
-exit;
 // Autoloader
-require '/../vendor/autoload.php';
+require '/vendor/autoload.php';
 
 
 
 // Load configuration
-$config = require  '/../src/config.php';
+$config = require  '/src/config.php';
 
 // Instantiate and run the application
 $app = new App\Core\Application($config);
